eef861b4a04deacf9d5d8bca8be6a43ceebb4b3d
Nikhil <nikhiljha4056@gmail.com>
2022-11-21 10:29:36 +0530

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Products } from './Products'
+import {Navbar} from './Navbar'
+
+export const Home = () => {
+  return (
+    <>
+        <Navbar/>
+        <Products/>
+    </>
+  )
+}
---
+++ src/Components/Products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Products = () => {
+  return (          
+    <div>
+        individual product
+        individual product 2
+    </div>
+  )         
+}
